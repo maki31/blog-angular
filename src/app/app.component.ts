6ac57c6c4c0e5024cf08b422c8e8e1234b5379f0
Christophe <makidev31@gmail.com>
2018-12-10 23:04:27 +0100

    edit: add like et dislike.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projet-blog';
+
+
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(() => {
+        resolve(date);
+      }, 2000);
+    }
+  )
+
+  posts = [
+    {
+      title: 'Article 1',
+      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores libero necessitatibus repudiandae ratione reprehenderit quo delectus? Velit reprehenderit voluptatem commodi facere nihil libero id. Repellat aut nihil ipsa aliquam explicabo?',
+      loveIts: 0,
+      created: this.lastUpdate
+    },
+    {
+      title: 'Article 2',
+      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores libero necessitatibus repudiandae ratione reprehenderit quo delectus? Velit reprehenderit voluptatem commodi facere nihil libero id. Repellat aut nihil ipsa aliquam explicabo?',
+      loveIts: 0,
+      created: this.lastUpdate
+    },
+    {
+      title: 'Article 3',
+      content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores libero necessitatibus repudiandae ratione reprehenderit quo delectus? Velit reprehenderit voluptatem commodi facere nihil libero id. Repellat aut nihil ipsa aliquam explicabo?',
+      loveIts: 0,
+      created: this.lastUpdate
+    }
+  ];
+
+  
+
+
+
+
+
 }
